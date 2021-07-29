07221eec95ec62b9e3318705d8c2459f11300a80
Krystian Dużyński <krystian.duzynski@gmail.com>
2021-07-29 14:36:47 +0200

    example: improved logging testing

--- KDWebServer.NET.Example/Program.cs
+++ KDWebServer.NET.Example/Program.cs
@@ -1,21 +1,57 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using KDLib;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NLog;
 using NLog.Config;
+using NLog.LayoutRenderers;
 using NLog.Targets;
 
 namespace KDWebServer.Example
 {
   class Program
   {
+    public static string FormatForLog(object val, bool multiline, bool keepShort)
+    {
+      return val switch {
+          null => "<null>",
+          string v => v,
+          IPEndPoint v => v.ToString(),
+          IPAddress v => v.ToString(),
+          NameValueCollection v => v.ToString(),
+          JToken v => v.ToString(multiline ? Formatting.Indented : Formatting.None),
+          Enum v => v.ToString(),
+          _ => FormatForLog(JToken.FromObject(val), multiline, keepShort),
+      };
+    }
+
     static async Task Main(string[] args)
     {
       var config = new LoggingConfiguration();
 
+      LayoutRenderer.Register("parameters", logEventInfo => {
+        if (logEventInfo.Properties.ContainsKey("CallerMemberName")) logEventInfo.Properties.Remove("CallerMemberName");
+        if (logEventInfo.Properties.ContainsKey("CallerFilePath")) logEventInfo.Properties.Remove("CallerFilePath");
+        if (logEventInfo.Properties.ContainsKey("CallerLineNumber")) logEventInfo.Properties.Remove("CallerLineNumber");
+
+        var parametersStr =
+            NLog.MappedDiagnosticsLogicalContext.GetNames().Select(key => (key, value: NLog.MappedDiagnosticsLogicalContext.GetObject(key)))
+                .Concat(logEventInfo.Properties.Select(x => (key: (string)x.Key, value: x.Value)))
+                .Distinct(x => x.key)
+                .Select(x => $"{x.key}={FormatForLog(x.value, false, keepShort: true)}")
+                .JoinString(", ");
+
+        return parametersStr.Length == 0 ? "" : $"({parametersStr})";
+      });
+
       var consoleTarget = new ConsoleTarget {
-          Layout = @"[${date:format=yyyy-MM-dd HH\:mm\:ss.fff}] [${logger}] ${message} ${exception:format=toString}",
+          Layout = @"[${date:format=yyyy-MM-dd HH\:mm\:ss.fff}] [${logger}] ${message} ${parameters} ${exception:format=toString}",
       };
 
       config.AddRuleForAllLevels(consoleTarget);
